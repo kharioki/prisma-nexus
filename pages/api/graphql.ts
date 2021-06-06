cac2c34ea3dfa951d67c2dd7576fa9c5e7674e5b
Tony Kharioki <antolisah@gmail.com>
2021-06-06 06:55:15 +0300

    creating context

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,8 +2,9 @@
 
 import { ApolloServer } from 'apollo-server-micro';
 import { schema } from 'src/schema';
+import { createContext } from 'src/context';
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ schema, context: createContext });
 const handler = server.createHandler({ path: '/api/graphql' });
 
 export const config = {
