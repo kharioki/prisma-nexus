f749712a67e43ec048b1f7b51cd5597325d77818
Tony Kharioki <antolisah@gmail.com>
2021-06-06 09:09:44 +0300

    schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,48 @@
-import { makeSchema, queryType } from 'nexus';
+import { makeSchema, queryType, objectType, idArg } from 'nexus';
+import path from 'path';
+
+const Company = objectType({
+  name: 'Company',
+  definition(t) {
+    t.nonNull.int('id');
+    t.nonNull.string('symbol');
+    t.nonNull.string('name');
+    t.nonNull.string('description');
+  }
+});
 
 const Query = queryType({
   definition(t) {
-    t.string('name', { resolve: () => 'Tony Kharioki' });
+    t.field('company', {
+      type: Company,
+      // nullable: true,
+      args: {
+        id: idArg(),
+      },
+      resolve: (_root, { id }, ctx) => {
+        return ctx.prisma.company.findUnique({ where: { id: Number(id) } })
+      },
+    });
   },
 });
+
 export const schema = makeSchema({
-    types: { Query }
+  types: { Query, Company },
+  outputs: {
+    schema: path.join(process.cwd(), 'schema.graphql'),
+    typegen: path.join(process.cwd(), 'nexus.ts')
+  },
+  typegenAutoConfig: {
+    contextType: 'Context.Context',
+    sources: [
+      {
+        source: '@prisma/client',
+        alias: 'prisma',
+      },
+      {
+        source: require.resolve('./context'),
+        alias: 'Context',
+      },
+    ],
+  }
 });
